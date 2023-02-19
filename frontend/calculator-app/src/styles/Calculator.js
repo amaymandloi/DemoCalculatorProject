651d783c176172275c3f573239e7f3dc1d538d54
amay mandloi <amaymandloi123@gmail.com>
2023-02-19 15:02:02 +0530

    initial commit

---
+++ frontend/calculator-app/src/styles/Calculator.js
@@ -0,0 +1,230 @@
+import React, { useState } from "react";
+import { Container, Screen, Prevoius, Current, Button } from "./style";
+import axios from "axios";
+import base_url from "../baseapi/baseapi";
+
+const Calculator = () => {
+  const [current, setCurrent] = useState("");
+  const [prevoius, setPrevoius] = useState("");
+  const [operations, setOperations] = useState("");
+  //const [result, setResult] = useState("");
+
+  const appendValueHandler = (el) => {
+    console.log("btn click");
+    const value = el.target.getAttribute("data");
+    if (value === "." && current.includes(".")) return;
+    setCurrent(current + value);
+  };
+
+  const deleteHandler = () => {
+    setCurrent(String(current).slice(0, -1));
+  };
+
+  const allclearHandler = () => {
+    setCurrent("");
+    setOperations("");
+    setPrevoius("");
+    //  setResult("");
+  };
+
+  const chooseOperationHandler = (el) => {
+    if (current === "") return;
+    if (prevoius !== "") {
+      let value = compute();
+      setPrevoius(value);
+    } else {
+      setPrevoius(current);
+    }
+    setCurrent("");
+    setOperations(el.target.getAttribute("data"));
+  };
+
+  const percentage = async (el) => {
+    if (current === "") return;
+    if (prevoius !== "") {
+      let value = compute();
+      setPrevoius(value);
+    } else {
+      setPrevoius(current);
+    }
+    setCurrent("");
+    setOperations(el.target.getAttribute("data"));
+  };
+
+  const squareOfNumber = async (e1) => {
+    if (current === "") return;
+    // console.log("square");
+    const square = await axios.get(`${base_url}square/${current}`);
+    setCurrent(square.data.toString());
+  };
+
+  const squareRootOfNumber = async (e1) => {
+    if (current === "") return;
+    // console.log("square");
+    const square = await axios.get(`${base_url}squareRoot/${current}`);
+    setCurrent(square.data.toString());
+  };
+
+  const numberByOne = async (e1) => {
+    if (current === "") return;
+    console.log("numberByOne");
+    const result = await axios.get(`${base_url}numberByOne/${current}`);
+    setCurrent(result.data.toString());
+  };
+
+  const equalHandler = () => {
+    let value = compute();
+    console.log(`value is ${value}`);
+    if (value === undefined || value == null) return;
+    // setCurrent(value);
+    console.log(`value curent ${setCurrent}`);
+    setPrevoius("");
+    setOperations("");
+  };
+
+  const compute = async () => {
+    // let result=parseFloat(result1);
+    let num1 = parseFloat(prevoius);
+    let num2 = parseFloat(current);
+    if (isNaN(num1) || isNaN(num2)) return;
+    switch (operations) {
+      case "/":
+        const divresult = await axios.get(
+          `${base_url}division/${num1}/${num2}`
+        );
+        setCurrent(divresult.data.toString());
+        break;
+
+      case "X":
+        const mulresult = await axios.get(
+          `${base_url}multification/${num1}/${num2}`
+        );
+        setCurrent(mulresult.data.toString());
+        break;
+
+      case "+":
+        const sumresult = await axios.get(
+          `${base_url}addition/${num1}/${num2}`
+        );
+        setCurrent(sumresult.data.toString());
+        break;
+
+      case "-":
+        const subresult = await axios.get(
+          `${base_url}subtractionion/${num1}/${num2}`
+        );
+        setCurrent(subresult.data.toString());
+        break;
+
+      case "%":
+        console.log("Percentage");
+        const percentage = await axios.get(
+          `${base_url}percentage/${num1}/${num2}`
+        );
+        setCurrent(percentage.data.toString());
+        break;
+      default:
+        return;
+    }
+
+    // return result;
+  };
+
+  //console.log(compute());
+
+  return (
+    // memory:null,
+    <>
+      <Container>
+        <Screen>
+          <Prevoius>
+            {prevoius}
+            {operations}
+          </Prevoius>
+          <Current>{current}</Current>
+        </Screen>
+
+        <Button>MR</Button>
+        <Button>M+</Button>
+        <Button>M-</Button>
+        <Button>MC</Button>
+        <Button operation data={"1/x"} onClick={numberByOne}>
+          1/x
+        </Button>
+        <Button operation data={"√"} onClick={squareRootOfNumber}>
+          ²√x
+        </Button>
+        <Button operation data={"%"} onClick={percentage}>
+          %
+        </Button>
+
+        <Button control onClick={allclearHandler}>
+          AC
+        </Button>
+        <Button operation data={"x²"} onClick={squareOfNumber}>
+          x²
+        </Button>
+        <Button operation data={"-"} onClick={chooseOperationHandler}>
+          -
+        </Button>
+        <Button operation data={"+"} onClick={chooseOperationHandler}>
+          +
+        </Button>
+        <Button operation data={"/"} onClick={chooseOperationHandler}>
+          /
+        </Button>
+        <Button data={1} onClick={appendValueHandler}>
+          1
+        </Button>
+        <Button data={2} onClick={appendValueHandler}>
+          2
+        </Button>
+        <Button data={3} onClick={appendValueHandler}>
+          3
+        </Button>
+
+        <Button operation data={"X"} onClick={chooseOperationHandler}>
+          X
+        </Button>
+        <Button data={4} onClick={appendValueHandler}>
+          4
+        </Button>
+        <Button data={5} onClick={appendValueHandler}>
+          5
+        </Button>
+        <Button data={6} onClick={appendValueHandler}>
+          6
+        </Button>
+
+        <Button del onClick={deleteHandler}>
+          ⌫
+        </Button>
+
+        <Button data={7} onClick={appendValueHandler}>
+          7
+        </Button>
+
+        <Button data={8} onClick={appendValueHandler}>
+          8
+        </Button>
+        <Button data={9} onClick={appendValueHandler}>
+          9
+        </Button>
+
+        <Button blank onClick={deleteHandler}></Button>
+        <Button decimal data={"."} onClick={appendValueHandler}>
+          .
+        </Button>
+        <Button data={0} onClick={appendValueHandler}>
+          0
+        </Button>
+
+        <Button equals onClick={equalHandler}>
+          =
+        </Button>
+      </Container>
+    </>
+  );
+};
+
+export default Calculator;
---
+++ frontend/calculator-app/src/styles/style.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  margin: 40px auto;
+  grid-template-columns: repeat(4, 110px);
+  grid-template-rows: minmax(120px, auto) repeat(7, 80px);
+  box-shadow: 2px 2px 10px #333;
+  border-radius: 10px;
+
+  width: 500px;
+  height: 700px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #485461;
+  background-image: linear-gradient(315deg, #485461 0%, #28313b 74%);
+`;
+
+export const Screen = styled.div`
+  grid-column: 1 / -1;
+  flex-direction: column;
+  word-wrap: break-word;
+  word-break: break-all;
+  text-align: right;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  margin-top: 10px;
+  margin-align: center;
+
+  margin-right: 30px;
+  height: 90px;
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 0 10px;
+  background-color: #4357692d;
+  border-radius: 10px;
+  display: flex;
+
+  justify-content: flex-end;
+  color: white;
+  font-weight: bold;
+  box-sizing: border-box;
+`;
+
+export const Prevoius = styled.div`
+  color: rgba(255, 255, 255, 0.75);
+  font-size: 1rem;
+`;
+
+export const Current = styled.div`
+  color: white;
+  font-size: 1.5rem;
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+  font-size: 2rem;
+  outline: none;
+  border: 1px outset white;
+  // border-radius: 20px;
+
+  background-color: rgba(255, 255, 255, 0.75);
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+
+  ${({ blank }) =>
+    blank &&
+    `background-color:#00000000;border: none; &:hover {
+    background-color:#00000000;
+  }`};
+  ${({ operation }) => operation && `background-color:gray;`};
+  ${({ control }) => control && `background-color:skyBlue;`};
+  ${({ del }) => del && `background-color:skyBlue;height:160px`};
+  ${({ equals }) =>
+    equals &&
+    `background-color:gray;border-bottom-right-radius:10px;width:220px`};
+  ${({ decimal }) =>
+    decimal && `background-color:gray;border-bottom-left-radius:10px;`};
+`;
